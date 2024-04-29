b5c4cca855e6fa3be8618ff526ac4f57fe9a9443
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-04-29 20:03:59 +0600

    M-7.5

--- Module_7.5/operator_overloading.py
+++ Module_7.5/operator_overloading.py
@@ -14,9 +14,25 @@ class Cricketer(Person):
     def __init__(self, name, age, height, weight) -> None:
         super().__init__(name, age, height, weight)
 
+lst =[]
+
 sakib = Cricketer('Sakib', 38, 68, 91)
 musfiq = Cricketer('Rahim', 36, 68, 88)
 kamal = Cricketer('Kamal', 39, 68, 94)
 jack = Cricketer('Jack', 38, 68, 91)
 kalam = Cricketer('Kalam', 37, 68, 95)
 
+lst.append(sakib)
+lst.append(musfiq)
+lst.append(kamal)
+lst.append(jack)
+lst.append(kalam)
+
+mostAge=0
+x = None
+for item in lst:
+    if(item.age > mostAge):
+        mostAge = item.age
+        x = item
+
+print(x.name)
